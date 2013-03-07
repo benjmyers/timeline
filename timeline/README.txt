606917dd0037229e8e759c75b43172516fe4cd1b
M Myers <myers.mm89@gmail.com>
2013-03-06 23:03:48 -0500

    Cleaning up

--- timeline/README.txt
+++
@@ -1,3 +0,0 @@
-README
-
-Timeline is a Ruby on Rails application intended to allow users to easily visualize and organize time dependent information for educational or professional usage.
--- timeline/db/schema.rb
+++ timeline/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130217015442) do
+ActiveRecord::Schema.define(:version => 20130307015054) do
 
   create_table "events", :force => true do |t|
     t.string   "title"
@@ -20,10 +20,12 @@ ActiveRecord::Schema.define(:version => 20130217015442) do
     t.string   "time"
     t.datetime "created_at",         :null => false
     t.datetime "updated_at",         :null => false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
-    t.string   "image_content_type"
-    t.string   "image_file_name"
+    t.integer  "image_height"
+    t.integer  "image_width"
   end
 
 end
