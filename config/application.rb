3ba986c42e8dad08f6ef87d1112a4c1de63c6a92
M Myers <myers@myersmeredith.com>
2013-04-13 10:38:21 -0400

    trying to resolve heroku precompile errors

--- config/application.rb
+++ config/application.rb
@@ -59,6 +59,7 @@ module Timeline
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     config.assets.initialize_on_precompile = false
+    config.assets.compress = false
 
   end
 end
